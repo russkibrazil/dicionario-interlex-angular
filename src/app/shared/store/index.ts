447a128678e5c82ae6fd7f7268105b7507e151cb
Igor Pereira <igorp9971@gmail.com>
2019-11-19 20:32:01 -0300

    Criação de seletores

--- src/app/shared/store/index.ts
+++ src/app/shared/store/index.ts
@@ -1,5 +1,10 @@
-import * as fromAuth from '../auth/store/auth.reducer';
 import { createSelector } from '@ngrx/store';
+import * as fromAuth from '../auth/store/auth.reducer';
+import * as fromEquivalente from '../store/equivalente.reducer';
+import * as fromFraseologia from '../store/fraseologia.reducer';
+import * as fromPalavras from '../store/palavras.reducer';
+import * as fromReferencias from '../store/referencias.reducer';
+import * as fromUsuarios from '../store/usuarios.reducer';
 
 export interface FeatureState{
     auth:fromAuth.State
@@ -7,8 +12,20 @@ export interface FeatureState{
 
 export interface AppState{
     ft: FeatureState;
+    equivalente: fromEquivalente.State,
+    fraseologia: fromFraseologia.State,
+    palavras: fromPalavras.State,
+    referencias: fromReferencias.State,
+    usuarios: fromUsuarios.State
 }
 
 export const selectFeature = (state: AppState) => state.ft;
+export const selectAuth = createSelector(
+    selectFeature, (state : FeatureState) => state.auth);
 
-export const selectAuth = createSelector(selectFeature, (state : FeatureState) => state.auth);
+export const selectEquivalente = (state:AppState) => state.equivalente;
+export const selectFraseologia = (state:AppState) => state.fraseologia;
+export const selectPalavras = (state:AppState) => state.palavras;
+export const selectReferencias = (state:AppState) => state.referencias;
+export const selectUsuarios = (state:AppState) => state.usuarios;
+
